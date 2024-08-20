697fa7c6d9d5083e10d63a643c991318cc2edc9f
Fabián Huertas <miloh9318@hotmail.com>
2024-08-19 20:25:29 -0500

    agregando mi foto y html terminado

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "bright-lime": "#c4f82a",
+        "color-background": "#141414",
+        "gray-color": "#333333",
+        "dark-gray": "#1f1f1f",
+        "very-dark-gray": "#202020" 
+      }
+    },
   },
   plugins: [],
 }
